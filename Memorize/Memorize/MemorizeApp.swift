a751eb596ed09c2b001d7ab9d6413129d89c9564
CareyWYR <wyr95626@163.com>
2022-10-09 14:31:01 +0800

    Memorize-init

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by CareyWYR on 2022/10/9.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
